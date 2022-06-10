b38ed9e277a11dd38dc682bcda7579da6fa07913
Stephen Zou <zou.stephen@gmail.com>
2022-06-10 17:24:55 +1200

    remove test file

--- exo_export.py
+++ exo_export.py
@@ -1,9 +1,15 @@
 from exo import ExoReport
 from exo.clarity import config
 
+done = ['BRANCHES', 'CREDIT_STATUS', 'CURRENCIES', 
+        'DR_ACCGROUP2S', 'DR_ACCGROUPS', 'DR_ACCS', 
+        'DR_ALLOCATIONS', 'DR_INVLINES', 'DR_TRANS', 
+        'NARRATIVES', 'PERIOD_STATUS', 'SALESORD_HDR', 'SALESORD_LINES', 'STAFF', 'STOCK_GROUPS',
+        'STOCK_ITEMS', 'X_DR_ACCGROUP3S', 'X_RETAILGROUP']
 
-rebate = ExoReport("NZ", params=None, report_type='DR_ACCS')
+reports = ['Z_GLTRANS']
 
-print(config)
+for report in reports:
+    ExoReport("NZ", params=None, report_type=report).save_csv()
 
-rebate.save_csv()
+#print(config)
